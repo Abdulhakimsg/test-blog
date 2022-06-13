7e265ee993d176aff5f71cb662676d42debac40e
hakimabdul <abdul.hakim@accredify.io>
2022-06-13 23:43:48 +0800

    additional routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::get('/', function () {
+//    return view('posts', [
+//        'posts' => Post::with('category')->get()
+//    ]);
+//});
+
+//Route::get('posts/{post}', function ($slug){
+//    return view('post', [
+//        'post' => Post::findOrFail($slug)
+//    ]);
+//});
+
+//Route::get('categories/{category:slug}', function( Category $category){
+//    return view('posts', ['posts' => $category->posts]);
+//});
